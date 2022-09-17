3615070d5633f84af5e9eb304e34edfd979553cb
cristhian vargas <cristhian.a.vargas@gmail.com>
2022-09-17 13:25:10 -0500

    Implement reducers for adding and removing transactions funtcion

--- src/components/AddTransaction/index.jsx
+++ src/components/AddTransaction/index.jsx
@@ -1,11 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { GlobalContext } from "../../context/GlobalState";
 import { Button, Title } from "../../styles/GlobalStyles";
 import { Form, FormInput, FormItem, FormLabel } from "./styles";
 
 export const AddTransaction = () => {
   const [text, setText] = useState("");
   const [amount, setAmount] = useState(0);
-
+  const {addTransaction} = useContext(GlobalContext)
   const handleChangeText = (e) => {
     console.log(e.target.value)
     setText(e.target.value)
@@ -16,10 +17,23 @@ export const AddTransaction = () => {
     setAmount(e.target.value)
   };
 
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    const newTransaction = {
+      id: Math.floor(Math.random() * 10000000),
+      text,
+      amount: Number(amount)
+    }
+    addTransaction(newTransaction)
+    setText("")
+    setAmount(0)
+  }
+
   return (
     <>
       <Title>Add new transaction</Title>
-      <Form>
+      <Form onSubmit={onSubmit}>
         <FormItem>
           <FormLabel htmlFor="text">Text</FormLabel>
           <FormInput type="text" id="text" value={text} onChange={handleChangeText} placeholder="Enter text..." />
